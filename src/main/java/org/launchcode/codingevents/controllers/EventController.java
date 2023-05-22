406a94ca165e30a58783ac10a81d70b2c2de25c4
Delaney <delaney.rosa1@gmail.com>
2023-05-21 21:06:18 -0500

    Finished ch 15 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -21,6 +21,7 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
+
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
